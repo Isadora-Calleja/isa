eada5f701f72fd42b5d1f8c2ea375ec6834e8cb8
Isadora-Calleja <105411272+Isadora-Calleja@users.noreply.github.com>
2022-08-21 04:53:17 -0500

    Add files via upload

--- json.php
+++ json.php
@@ -3,5 +3,5 @@
 use function PHPSTORM_META\type;
 
 header("content-type:application/json");
-echo("autor":"Miguel de Cervantes" , "cantidad":"16");
+echo '("autor":"Miguel de Cervantes" , "cantidad":"16")';
 ?>
